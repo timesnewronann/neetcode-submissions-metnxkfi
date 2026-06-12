faacbf71516699c46d33c8de6d70853bf366dab2
Ronan Jared Borja <72775456+timesnewronann@users.noreply.github.com>
2026-06-11 18:04:22 -0700

    Add: remove-element - submission-18

---
+++ Data Structures & Algorithms/remove-element/submission-18.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # we can use two pointers
+        left = 0 
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[left] = nums[i]
+                left += 1 
+        
+        return left
