9af198c213459c89175bf15c6b8d6434b14cbf32
Ronan Jared Borja <72775456+timesnewronann@users.noreply.github.com>
2026-07-01 18:35:18 -0700

    Add: python-zip - submission-0

---
+++ Python Coding Interviews/python-zip/submission-0.py
@@ -0,0 +1,16 @@
+from typing import List, Dict
+
+
+def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:
+    names_and_scores = {}
+
+    for name, score in zip(names, scores):
+        names_and_scores[name] = score
+
+    return names_and_scores
+
+
+# do not modify below this line
+print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))
