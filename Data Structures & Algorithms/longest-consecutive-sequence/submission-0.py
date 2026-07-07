c10a1bfc98d4b7e52961a7f0be40cfa95f54243c
Ronan Jared Borja <72775456+timesnewronann@users.noreply.github.com>
2026-07-06 18:13:52 -0700

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # goal: return the length of the longest consecutive sequence
+        # What is a consecutive sequence?
+        # each element is exactly 1 greater than the previous element
+        # elements do not have to be consecutive in the original array
+        # We would check if nums[i] 1 greater than nums[i -1]
+        # ex: nums[i] == 1, nums[i + 1] == 2
+        # we want to use a set to avoid having to sort this problem
+
+        # use the set to check the next number in a sequence
+        hashSet = set(nums)
+
+        # keep track of the longest consecutive sequence
+        longest = 0
+
+        for num in nums:
+            # check if it is the start of the sequence
+            if (num -1) not in hashSet:
+                length = 0 
+
+                while (num + length) in hashSet: # check the current number
+                    length += 1 
+                
+                longest = max(longest, length)
+
+        return longest
