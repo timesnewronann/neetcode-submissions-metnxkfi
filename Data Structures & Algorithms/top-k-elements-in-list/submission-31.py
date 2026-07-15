83dee112f95980816e5eca14a6fdbcedc08cd266
Ronan Jared Borja <72775456+timesnewronann@users.noreply.github.com>
2026-07-14 19:26:54 -0700

    Add: top-k-elements-in-list - submission-31

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-31.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # we can track the groups with a dictionary
+        counts = defaultdict(int)
+
+        for num in nums:
+            counts[num] += 1 
+        
+        buckets = [[] for _ in range(len(nums) + 1)] 
+
+        for num, count in counts.items():
+            buckets[count].append(num)
+
+        result = []
+
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                result.append(num)
+
+                if len(result) == k:
+                    return result
+
