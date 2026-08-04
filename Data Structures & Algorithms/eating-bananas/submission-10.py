0b9528a34bc557dc39aab378d29d8e64ca5f2f10
Ronan Jared Borja <72775456+timesnewronann@users.noreply.github.com>
2026-08-03 21:07:33 -0700

    Add: eating-bananas - submission-10

---
+++ Data Structures & Algorithms/eating-bananas/submission-10.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # I think we can use a binary search to solve this question
+        # We would adjust our eating rate based on the hours 
+
+        # the smallest rate of bannana eating
+        lowest = 1 
+
+        # highest rate of bannana eating
+        highest = max(piles)
+
+        # track the result of our bannana eating rate
+        result = highest
+
+        while lowest <= highest:
+            # track the amount of hours it takes to eat with our current rate
+            hours = 0 
+            k = (lowest + highest) // 2
+
+            # we should go through each pile of bannana's now
+            for pile in piles:
+                # hours is our the amount in the pile / banana eating rate
+                # add up our hours it takes to eat the piles
+                hours += math.ceil(pile / k)
+
+            if hours <= h:
+                result = min(result, k)
+                highest = k - 1 
+                
+            else:
+                lowest = k + 1 
+
+
+        return result
