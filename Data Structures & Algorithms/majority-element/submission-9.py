3c5bb4f8cc492248ad0b13714db975cecb6d5d8f
Ronan Jared Borja <72775456+timesnewronann@users.noreply.github.com>
2026-08-07 19:51:37 -0700

    Add: majority-element - submission-9

---
+++ Data Structures & Algorithms/majority-element/submission-9.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # maybe we can use a hashMap track the counts of each num
+        result = 0
+
+        count = 0 
+
+        for num in nums:
+            if count == 0:
+                result = num
+
+            if num == result:
+                count += 1 
+            
+            else:
+                count -= 1 
+
+        return result
+
+
+        
+                
