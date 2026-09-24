e38391ea1b7fb78663f69a00796de2ad6e25b6bb
Ronan Jared Borja <72775456+timesnewronann@users.noreply.github.com>
2026-09-23 21:53:48 -0700

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1 
+
+        while left < right:
+            # check if the character is alnum
+            if not s[left].isalnum():
+                left += 1 
+            
+            if not s[right].isalnum():
+                right -= 1 
+
+            if s[left].isalnum() and s[right].isalnum():
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1 
+                right -= 1 
+
+        return True
